f58e12f3592b7bb6ccfadbb2d30f75fceae358d0
Justyna Gietka <justyna.gietka@gmail.com>
2019-01-11 12:31:33 +0000

    modified sendEmail function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
-function sendMail(contactForm) {
-    emailjs.send ("gmail", "nanny", { //method emailjs.send has 3 aguments, gmail, nanny and form (form consists of 3 parameters)
+function sendMail(contactForm) { //method emailjs.send has 3 aguments, gmail, nanny and form (form consists of 3 parameters)
+    emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
@@ -8,7 +8,9 @@ function sendMail(contactForm) {
         function(response) {
             console.log("SUCCESS", response);
         },
-        function(error){
-            console.log("ERROR", error);
-        });  
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
 }
